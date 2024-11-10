737b5339c8b4a8b8afe8a862467ab7e3d8b18f53
Cabarrubias-Ryan <cabarrubias1002@gmail.com>
2024-11-10 08:40:33 +0800

    Changes of database/models

--- app/Models/Sales.php
+++ app/Models/Sales.php
@@ -5,16 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Sales extends Model
 {
   use SoftDeletes;
-  protected $table = 'product';
+  protected $table = 'sales';
   protected $fillable = [
     'id',
-    'product_code',
-    'name',
-    'description',
+    'date',
+    'amount',
+    'order_id',
+    'iser_id',
+    'product_id',
     'discount',
+    'status',
+    'net_amount',
     'created_at',
     'updated_at',
     'deleted_at',
